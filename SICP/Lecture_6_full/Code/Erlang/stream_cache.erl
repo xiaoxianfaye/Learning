e2e9d2a85a4de06dc7302f00a8223851cf553d3f
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2017-05-26 17:20:12 +0800

    stream_p

--- SICP/Lecture_6_full/Code/Erlang/stream_cache.erl
+++ SICP/Lecture_6_full/Code/Erlang/stream_cache.erl
@@ -7,17 +7,18 @@
 -spec cons_stream(term(), fun(() -> stream())) -> stream().
 cons_stream(H, T) ->
     Ref = make_ref(),
-    io:format("make_ref ~p~n", [Ref]),
+    % io:format("make_ref ~p~n", [Ref]),
     F = fun() ->
-            io:format("Ref ~p~n", [Ref]),
+            % io:format("Ref ~p~n", [Ref]),
             case get(Ref) of
                 undefined ->
                     Res = T(),
                     put(Ref, Res),
-                    io:format("put {~p, ~p}~n", [Ref, Res]),
+                    % io:format("put {~p, ~p}~n", [Ref, Res]),
                     Res;
                 R ->
-                    io:format("get {~p, ~p}~n", [Ref, R]),
+                    % io:format("get {~p, ~p}~n", [Ref, R]),
+                    put(cnt, get(cnt) + 1),
                     R
             end
         end,
@@ -30,6 +31,22 @@ the_empty_stream() -> [].
 
 is_empty_stream([]) -> true;
 is_empty_stream(_) -> false.
+
+add_stream(S1, S2) ->
+    case {is_empty_stream(S1), is_empty_stream(S2)} of
+        {true, true} ->
+            the_empty_stream();
+        {true, false} ->
+            S2;
+        {false, true} ->
+            S1;
+        {false, false} ->
+            cons_stream(
+                head(S1) + head(S2),
+                fun() ->
+                    add_stream(tail(S1), tail(S2))
+                end)
+    end.
 
 %% Terminals
 nth_stream(Idx, S) ->
@@ -45,7 +62,45 @@ nth_stream(Idx, S) ->
             end
     end.
 
+print_stream(_S, 0) -> ok;
+print_stream(S, N) ->
+    case is_empty_stream(S) of
+        true ->
+            io:format("Done~n");
+        false ->
+            io:format("~p ", [head(S)]),
+            print_stream(tail(S), N - 1)
+    end.    
+
 %% Infinite Stream
 integers_from(N) ->
     cons_stream(N, fun() -> integers_from(N + 1) end). 
 
+% Defining streams implicitly
+fibs() ->
+    cons_stream(0,
+                fun() ->
+                    cons_stream(1,
+                                fun() ->
+                                    add_stream(fibs(), tail(fibs()))
+                                end)
+                end).
+
+%% Pseudo simulation using process dictionary
+naming_cons_stream(N, H, T) ->
+    put(N, cons_stream(H, T)),
+    get(N).
+
+fibs_naming() ->
+    naming_cons_stream(
+        fibs,
+        0,
+        fun() ->
+            cons_stream(
+                1,
+                fun() ->
+                    Fibs = get(fibs),
+                    add_stream(Fibs, tail(Fibs))
+                end)
+        end
+    ).
---
+++ SICP/Lecture_6_full/Code/Erlang/stream_p.erl
@@ -0,0 +1,101 @@
+-module(stream_p).
+-compile(export_all).
+
+-type stream() :: pid() | [].
+-spec cons_stream(term(), fun((stream()) -> stream())) -> stream().
+
+cons_stream(H, T) ->
+    spawn(fun() -> stream_idle(H, T) end).
+
+head(S) ->
+    Ref = make_ref(),
+    S ! {ask_head, Ref, self()},
+    receive
+        {head, Ref, H} ->
+            H
+    end.
+
+tail(S) ->
+    Ref = make_ref(),
+    S ! {ask_tail, Ref, self()},
+    receive
+        {tail, Ref, T} ->
+            T
+    end.
+
+stream_idle(H, TF) ->
+    receive
+        {ask_head, Ref, Pid} ->
+            Pid ! {head, Ref, H},
+            stream_idle(H, TF);
+        {ask_tail, Ref, Pid} ->
+            S = self(),
+            spawn(
+                fun() ->
+                    TR = TF(S),
+                    S ! {tail_ready, TR}
+                end),
+            stream_tl_waiting(H, [{Pid, Ref}])
+    end.
+
+stream_tl_waiting(H, TWaits) ->
+    receive
+        {ask_head, Ref, Pid} ->
+            Pid ! {head, Ref, H},
+            stream_tl_waiting(H, TWaits);
+        {ask_tail, Ref, Pid} ->
+            stream_tl_waiting(H, [{Pid, Ref} | TWaits]);
+        {tail_ready, TR} ->
+            [Pid ! {tail, Ref, TR} || {Pid, Ref} <- TWaits],
+            stream_ready(H, TR)
+    end.
+
+stream_ready(H, TR) ->
+    receive
+        {ask_head, Ref, Pid} ->
+            Pid ! {head, Ref, H},
+            stream_ready(H, TR);
+        {ask_tail, Ref, Pid} ->
+            Pid ! {tail, Ref, TR},
+            stream_ready(H, TR)
+    end.
+
+the_empty_stream() -> [].
+
+is_empty_stream([]) -> true;
+is_empty_stream(_) -> false.
+
+add_stream(S1, S2) ->
+    case {is_empty_stream(S1), is_empty_stream(S2)} of
+        {true, true} ->
+            the_empty_stream();
+        {true, false} ->
+            S2;
+        {false, true} ->
+            S1;
+        {false, false} ->
+            cons_stream(
+                head(S1) + head(S2),
+                fun() ->
+                    add_stream(tail(S1), tail(S2))
+                end)
+    end.    
+
+%% Defining streams implicitly
+ones() ->
+    cons_stream(1, fun(S) -> S end).
+
+integers() ->
+    cons_stream(1, 
+                fun(I) ->
+                    add_stream(I, ones())
+                end).
+
+fibs() ->
+    cons_stream(0,
+                fun(S1) ->
+                    cons_stream(1,
+                                fun(S2) ->
+                                    add_stream(S1, S2)
+                                end)
+                end).
