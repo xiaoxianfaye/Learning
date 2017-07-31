f9ecf33159780ecd026bf4c58cad8a93786a437b
Faye Chen <xiaoxianfaye@hotmail.com>
2017-07-31 22:22:29 +0800

    sicp stream java

--- SICP/Lecture_6_full/Code/Java/Stream/src/fayelab/sicp/stream/pi/PiEstimatorWithDelayStream.java
+++ SICP/Lecture_6_full/Code/Java/Stream/src/fayelab/sicp/stream/pi/PiEstimatorWithDelayStream.java
@@ -1,16 +1,14 @@
 package fayelab.sicp.stream.pi;
 
+import static fayelab.sicp.stream.delaystream.StreamOp.*;
+
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
-
-import static fayelab.sicp.stream.delaystream.StreamOp.*;
 
 public class PiEstimatorWithDelayStream
 {
     private static final int MAX_RANDOM_NUMER = 100000;
-    
+
     private static List<Object> randomStream()
     {
         return consStream(randomNumber(), () -> randomStream());
@@ -28,14 +26,17 @@ public class PiEstimatorWithDelayStream
     
     private static List<Object> monteCarloStream(List<Object> s, int total, int passed)
     {
-        int newPassed = (Boolean)head(s) == true ? passed + 1 : passed;
+        int newPassed = (Boolean)head(s) ? passed + 1 : passed;
         int newTotal = total + 1;
-        return consStream(newPassed / newTotal * 1.0, () -> monteCarloStream(tail(s), newTotal, newPassed));
+        
+        System.out.println("newTotal = " + newTotal + " newPassed = " + newPassed + " p = " + 1.0 * newPassed / newTotal);
+        
+        return consStream(1.0 * newPassed / newTotal, () -> monteCarloStream(tail(s), newTotal, newPassed));
     }
     
-    static List<Object> piStream()
+    private static List<Object> piStream()
     {
-        return mapStream((Double p) -> p == 0.0 ? 0.0 : Math.sqrt(6. / p),
+        return mapStream((Double p) -> doubleEquals(p, 0.0) ? 0.0 : Math.sqrt(6. / p),
                          monteCarloStream(cesaroStream(randomStream()), 0, 0));
     }
     
@@ -67,28 +68,34 @@ public class PiEstimatorWithDelayStream
     private static double streamLimit(double prev, List<Object> s, double tolerance)
     {
         double next = head(s);
-        if(Math.abs(next - prev) < tolerance)
+        
+        System.out.println("next = " + next + " prev = " + prev);
+        
+        if(doubleEquals(next, prev, tolerance) && !doubleEquals(next, 2.449489742783178))
         {
             return next;
         }
+        
         return streamLimit(next, tail(s), tolerance);
     }
     
-    private static DoubleStream estimate(int...counts)
+    private static boolean doubleEquals(double d1, double d2)
     {
-        return IntStream.of(counts).mapToDouble(count -> estimate(count));
+        return doubleEquals(d1, d2, 0.000000000000001);
+    }
+    
+    private static boolean doubleEquals(double d1, double d2, double tolerance)
+    {
+        return Math.abs(d1 - d2) < tolerance;
     }
     
     public static void main(String[] args)
     {
-        System.out.println(PiEstimatorWithDelayStream.estimate(0.001));
+        System.out.println("****** " + PiEstimatorWithDelayStream.estimate(0.0001));
         
-        List<Object> pis = piStream();
-        System.out.println(nthStream(100, pis));
-        System.out.println(nthStream(1000, pis));
-        System.out.println(nthStream(10000, pis));
-        
-//        DoubleStream pis = PiEstimatorWithDelayStream.estimate(1000, 10000, 100000, 1000000, 10000000);
-//        pis.forEach((pi) -> System.out.println(pi));
+//        List<Object> pis = PiEstimatorWithDelayStream.piStream();
+//        System.out.println("****** 100th " + nthStream(100, pis));
+//        System.out.println("****** 1000th  " + nthStream(1000, pis));
+//        System.out.println("****** 10000th " + nthStream(10000, pis));
     }
 }
