e91f3d9bb6a769456858b007ec73dfcd64c4530a
Faye Chen <xiaoxianfaye@hotmail.com>
2017-08-02 19:41:34 +0800

    sicp stream python

--- SICP/Lecture_6_full/Code/Python/bif/requeens.py
+++ SICP/Lecture_6_full/Code/Python/bif/requeens.py
@@ -75,7 +75,7 @@ def test_solveall2():
     assert [[(1, 3), (2, 1), (3, 4), (4, 2)],
             [(1, 2), (2, 4), (3, 1), (4, 3)]] == solveall2(4)
     assert None == solveall2(3)
-    # assert 92 == len(solveall2(8))
+    assert 92 == len(solveall2(8))
 
     print 'test_solveall2 ok'    
 
