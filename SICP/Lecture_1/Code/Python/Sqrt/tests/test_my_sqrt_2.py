138f5a655f7fd3ebd697c2eda7ddc12457798f05
xiaoxianfaye <xiaoxianfaye@hotmail.com>
2019-02-20 22:45:59 +0800

    SICP Lecture 1 Hanoi in Python

--- SICP/Lecture_1/Code/Python/Sqrt/tests/test_my_sqrt_2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from sqrt.my_sqrt_2 import sqrt, TOLERANCE
-
-class TestMySqrt2(unittest.TestCase):
-    def test_sqrt(self):
-        self.assertAlmostEqual(2.0, sqrt(4.0), delta=TOLERANCE)
-        self.assertAlmostEqual(3.0, sqrt(9.0), delta=TOLERANCE)
